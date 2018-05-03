f3cee16cffdb203d9403c8316af4b90e80792062
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-03 16:40:06 +0200

    independants tests

--- test/ttt_new_game_spec.js
+++ test/ttt_new_game_spec.js
@@ -1,30 +1,33 @@
 describe('ttt_new_game function', function() {
-    it('should be defined', function() {
-      //given
-      //when
-      //then
-      expect(ttt_new_game).toBeDefined()
-    });
-    it('should create player 1', function() {
-      //given
-      //when
-      ttt_new_game()
-      //then
-      expect(window.my_player1).toBeDefined()
-    });
-    it('should create my_player1 as instanceof Player', function() {
-      //given
-      //when
-      ttt_new_game()
-      //then
-      expect(window.my_player1 instanceof Player).toEqual(true)
-    });
-    it('should have an "x" coin' , function() {
-      //given
-      //when
-      ttt_new_game()
-      //then
-      expect(window.my_player1.getCoinAppearence()).toEqual("x")
-    });
+  beforeEach(function() {
+    delete window.my_player1
+  })
+  it('should be defined', function() {
+    //given
+    //when
+    //then
+    expect(ttt_new_game).toBeDefined()
+  });
+  it('should create player 1', function() {
+    //given
+    //when
+    ttt_new_game()
+    //then
+    expect(window.my_player1).toBeDefined()
+  });
+  it('should create my_player1 as instanceof Player', function() {
+    //given
+    //when
+    ttt_new_game()
+    //then
+    expect(window.my_player1 instanceof Player).toEqual(true)
+  });
+  it('should have an "x" coin', function() {
+    //given
+    //when
+    ttt_new_game()
+    //then
+    expect(window.my_player1.getCoinAppearence()).toEqual("x")
+  });
 
-});    
+});
