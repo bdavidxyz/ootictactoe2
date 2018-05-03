06adcc5ecbc3757e684fb03318895128cdfc5534
David Boureau <bdavidxyz@gmail.com>
2018-05-03 14:13:39 +0200

    eslint ok

--- src/my/ttt_new_game.js
+++ src/my/ttt_new_game.js
@@ -1,8 +1,11 @@
+/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "ttt_new_game" }]*/
+
 function ttt_new_game() {
-var player1 = new Player('player1', 'x')
-var player2 = new Player('player2', 'o')
-var board = new Board()
-var game = new TicTacToe(player1, player2, board)
-game.start()
-return [player1, player2, board, game]
+  var player1 = new Player('player1', 'x')
+  var player2 = new Player('player2', 'o')
+  var board = new Board()
+  var game = new TicTacToe(player1, player2, board)
+  game.start()
+  return [player1, player2, board, game]
 }
+
