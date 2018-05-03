53b35493b01b5ef80c1721988202d0403bef1a5b
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-03 12:12:00 +0200

    add ttt_new_game and ttt_new_game_spec

---
+++ test/ttt_new_game_spec.js
@@ -0,0 +1,7 @@
+describe('ttt_new_game_spec.js', function() {
+  describe('ttt_new_game function', function() {
+    it('should be expected', function() {
+      expect(ttt_new_game).toBeDefined()
+    });
+  });    
+});
