8e3fdc3575a7045f8f31f417d25c65c4115ca2b9
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-03 16:01:23 +0200

    green test

--- test/ttt_new_game_spec.js
+++ test/ttt_new_game_spec.js
@@ -1,9 +1,15 @@
 describe('ttt_new_game function', function() {
     it('should be defined', function() {
+      //given
+      //when
+      //then
       expect(ttt_new_game).toBeDefined()
     });
     it('should create player 1', function() {
+      //given
+      //when
+      ttt_new_game()
+      //then
       expect(window.player1).toBeDefined()
-      //expect(ttt_new_game()[0] instanceof Player).toBe(true)
     });
 });    
