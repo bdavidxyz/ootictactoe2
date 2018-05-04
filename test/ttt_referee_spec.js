28fb5fb5ef37e3f362b2c0c4f2db0987867b5985
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-04 12:00:16 +0200

    green test

---
+++ test/ttt_referee_spec.js
@@ -0,0 +1,30 @@
+describe('ttt_referee_spec.js', function() {
+  it('Referee should be defined', function() {
+    //given
+    //when
+    //then
+    expect(Referee).toBeDefined()
+  });
+/*  it('getStatus instance method should be defined', function() {
+    //given
+    //when
+    var game = new TicTacToe();
+    //then
+    expect(game.getStatus).toBeDefined()
+  });
+  it('getReferee instance method should be defined', function() {
+    //given
+    //when
+    var game = new TicTacToe();
+    //then
+    expect(game.getReferee).toBeDefined()
+  });
+  it('getReferee instance method should return Referee class', function() {
+    //given
+    var game = new TicTacToe();
+    //when
+    var referee = game.getReferee()
+    //then
+    expect(referee instanceof Referee).toEqual(true)
+  });*/
+});
