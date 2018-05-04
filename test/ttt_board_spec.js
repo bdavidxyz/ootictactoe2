551cda8ad2615e09dd5db33fb78fc8fc8337d966
David Boureau <bdavidxyz@gmail.com>
2018-05-04 16:00:22 +0200

    refactored

--- test/ttt_board_spec.js
+++ test/ttt_board_spec.js
@@ -1,17 +1,29 @@
 describe('ttt_board_spec.js', function() {
   it('Board should be defined', function() {
+    //given
+    //when
+    //then
     expect(Board).toBeDefined()
   });
   it('Should be able to be instantiated', function() {
+    //given
+    //when
     var board = new Board()
+    //then
     expect(board instanceof Board).toEqual(true)
   });
   it('Should have the isFull function', function() {
+    //given
+    //when
     var board = new Board()
-    expect(board.isFull).toBeDefined();
+    //then
+    expect(_.isFunction(board.isFull)).toEqual(true);
   });
   it('Should not be full by default', function() {
+    //given
+    //when
     var board = new Board()
+    //then
     expect(board.isFull()).toEqual(false);
   });
 });
