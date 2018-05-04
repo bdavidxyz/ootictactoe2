da2e0d04ee2e47161a4bfc5098dda3559227b710
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-04 11:26:02 +0200

    deleted display spec

--- test/ttt_tictactoe_spec.js
+++ test/ttt_tictactoe_spec.js
@@ -34,32 +34,4 @@ describe('ttt_tictactoe_spec.js', function() {
     //then
     expect(game.getStatus()).toEqual("STARTED")
   });
-/*  it('Should be able to be asked', function() {
-    var asker = new Asker();
-    expect(asker.ask).toBeDefined()
-  });
-  it('Should call window.prompt when asking', function() {
-    spyOn(window, "prompt")
-    var asker = new Asker();
-    //when
-    asker.ask()
-    //then
-    expect(window.prompt).toHaveBeenCalled();
-  });
-  it('Should call window.prompt with same given arg, when asking', function() {
-    spyOn(window, "prompt")
-    var asker = new Asker();
-    //when
-    asker.ask("something")
-    //then
-    expect(window.prompt).toHaveBeenCalledWith("something");
-  });
-  it('Should return what the prompt returns', function() {
-    spyOn(window, "prompt").and.returnValue(42);
-    var asker = new Asker();
-    //when
-    var result = asker.ask()
-    //then
-    expect(result).toEqual(42);
-  });*/
 });
