69f6efe3da5643756b79f975fea8545079c7c190
maxdeval <maxime.devallez@pole-emploi.fr>
2018-05-04 11:17:48 +0200

    red test

--- test/ttt_tictactoe_spec.js
+++ test/ttt_tictactoe_spec.js
@@ -1,18 +1,38 @@
 describe('ttt_tictactoe_spec.js', function() {
   it('TicTacToe should be defined', function() {
+    //given
+    //when
+    //then
     expect(TicTacToe).toBeDefined()
   });
   it('getStatus instance method should be defined', function() {
+    //given
+    //when
     var game = new TicTacToe();
+    //then
     expect(game.getStatus).toBeDefined()
   });
   it('start instance method should be defined', function() {
+    //given
+    //when
     var game = new TicTacToe();
+    //then
     expect(game.start).toBeDefined()
   });
   it('should have status NOT_STARTED by default', function() {
+    //given
+    //when
     var game = new TicTacToe();
+    //then
     expect(game.getStatus()).toEqual("NOT_STARTED")
+  });
+  it('should have status STARTED when game is started', function() {
+    //given
+    var game = new TicTacToe();
+    //when
+    game.start()
+    //then
+    expect(game.getStatus()).toEqual("STARTED")
   });
 /*  it('Should be able to be asked', function() {
     var asker = new Asker();
